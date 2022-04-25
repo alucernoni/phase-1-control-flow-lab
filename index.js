7a1e4bbe8de9f9cec23bd3ed1a7289c1733af44f
Annemarie <a.lucernoni0@gmail.com>
2022-04-25 16:05:22 -0700

    commit tree dd3335cdee34fc2a026926e5498ec61d237fb5ea

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let customerMessage 
+  if (feet <= 400) { 
+    customerMessage = "This one is on me!";
+  }
+  else if (feet <= 2000) {
+    customerMessage = "That will be twenty bucks.";
+  }
+  else if (feet <=2500) {
+    customerMessage = "I will gladly take your thirty bucks."
+  }
+  else if (feet >2500) {
+    customerMessage = "No can do."
+  }
+  return customerMessage
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let goodCity;
+  city === "NYC" ? (goodCity = "Ok, sounds good.") : (goodCity = "No go.");
+  return goodCity;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipPercent){
   // Write your code here!
+  let partingMessage;
+  switch (tipPercent) {
+    case "generous":
+      partingMessage = "Thank you so much.";
+      break;
+    case "not as generous":
+      partingMessage = "Thank you.";
+      break;
+    default: 
+      partingMessage = "Bye.";
+      break;
+  }
+  return partingMessage;
 }
